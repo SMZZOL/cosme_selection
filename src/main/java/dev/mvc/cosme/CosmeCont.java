4ac3022e71ab0f2f591ffc538ffe99d844524655
stupidbutcute <dudwls138946@gmail.com>
2023-06-01 12:16:35 +0900

    update 2

--- src/main/java/dev/mvc/cosme/CosmeCont.java
+++ src/main/java/dev/mvc/cosme/CosmeCont.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class CosmeCont {
@@ -48,46 +53,68 @@ public class CosmeCont {
 		return "반환성공";
 	}
 	
-	/**
-	 * 등록 폼
-	 * http://localhost:9093/cosme/create.do
-	 * @param cosmeno
-	 * @return
-	 */
-	@RequestMapping(value="/cosme/create.do", method = RequestMethod.GET)
-	public ModelAndView create(int cosmeno) {
-	  ModelAndView mav = new ModelAndView();
-
-    mav.setViewName("/cosme/create"); 
+//	/**
+//	 * 등록 폼
+//	 * http://localhost:9093/cosme/create.do
+//	 * @param cosmeno
+//	 * @return
+//	 */
+//	@RequestMapping(value="/cosme/create.do", method = RequestMethod.GET)
+//	public ModelAndView create(int cosmeno) {
+//	  ModelAndView mav = new ModelAndView();
+//
+//    mav.setViewName("/cosme/create"); 
+//    
+//    return mav;
+//	  
+//    
+//    /**
+//     * 등록 처리
+//     * http://localhost:9093/cosme/create.do
+//     * @return
+//     */
+//    @RequestMapping(value="/cosme/create.do", method=RequestMethod.POST)
+//    public ModelAndView create(CosmeVO cosmeVO) {
+//      
+//      ModelAndView mav = new ModelAndView();
+//      mav.setViewName("/cosme/msg");
+//      
+//      int cnt = this.cosmeProc.create(cosmeVO);
+//      
+//      if (cnt == 1) {
+//        mav.addObject("code", "create_success");
+//      } else {
+//        mav.addObject("code", "create_fail");
+//      }
+//      
+//      mav.addObject("cnt", cnt);
+//      
+//      return mav;
+//    }
     
-    return mav;
-	  
-    
-    /**
-     * 등록 처리
-     * http://localhost:9093/cosme/create.do
-     * @return
-     */
-    @RequestMapping(value="/cosme/create.do", method=RequestMethod.POST)
-    public ModelAndView create(CosmeVO cosmeVO) {
-      
-      ModelAndView mav = new ModelAndView();
-      mav.setViewName("/cosme/msg");
-      
-      int cnt = this.cosmeProc.create(cosmeVO);
-      
-      if (cnt == 1) {
-        mav.addObject("code", "create_success");
-      } else {
-        mav.addObject("code", "create_fail");
-      }
-      
-      mav.addObject("cnt", cnt);
-      
-      return mav;
+    @GetMapping("/create")
+    public String createForm(Model model) {
+      model.addAttribute("title", "");
+      model.addAttribute("content", "");
+      model.addAttribute("word", "");
+      return "cosme/create";
     }
+
+    @PostMapping("/create")
+    public String create(
+        @RequestParam("title") String title,
+        @RequestParam("content") String content,
+        @RequestParam("word") String word,
+        @RequestParam("file") MultipartFile file) {
+      // 등록 처리 로직을 구현합니다.
+      // 받아온 데이터를 활용하여 데이터를 처리합니다.
+      // 필요한 비즈니스 로직을 수행한 후 적절한 응답을 반환합니다.
+
+      return "redirect:/cosme/list";
+    }
+
+    // 다른 필요한 핸들러 메서드를 추가로 구현할 수 있습니다.
+  
     
-    
-    
-	  }
+	  
 }
